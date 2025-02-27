0013fe55074a430c9725ecc3939518e8cea496f7
Saniya Jones <saniya.jones@esums.tech>
2025-02-27 19:28:53 +0000

    created black box webpage

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path("". views.hellotime, name="hellotime"),
+    path("", views.hellotime, name="hellotime"),
     path("screenprint", views.screenprint, name="screenprint"),
+    path("blackbox",views.blackbox, name="blackbox"),
 ]
